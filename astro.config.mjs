b071ed0ce41a03158875b62b14b816587f7d594a
Franklin Shera <fshera96@gmail.com>
2025-06-13 10:46:26 +0300

    add multiadapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,32 @@ import netlify from "@astrojs/netlify";
 
 import node from "@astrojs/node";
 
+import vercel from "@astrojs/vercel";
+
+function getAdapter() {
+  /**
+   * https://vercel.com/docs/environment-variables/system-environment-variables#VERCEL
+   */
+  if (process.env.VERCEL) {
+    console.log("Building for Vercel");
+
+    return vercel();
+  }
+
+  /**
+   * https://docs.netlify.com/configure-builds/environment-variables/#build-metadata
+   */
+  if (process.env.NETLIFY) {
+    console.log("Building for Netlify");
+    return netlify();
+  }
+
+  console.log("Building for Node");
+  return node({
+    mode: "middleware",
+  });
+}
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
@@ -36,9 +62,5 @@ export default defineConfig({
     },
   },
   output: "server",
-  adapter: process.env.RENDER
-    ? node({
-        mode: "standalone",
-      })
-    : netlify(),
+  adapter: getAdapter(),
 });
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,8 +1,7 @@
 /// <reference types="astro/client" />
-type NetlifyLocals = import("@astrojs/netlify").NetlifyLocals;
 
 declare namespace App {
-  interface Locals extends NetlifyLocals {
+  interface Locals {
     user: import("~/auth").Session["user"] | null;
     session: import("~/auth").Session | null;
   }
